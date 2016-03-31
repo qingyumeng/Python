a3f9dba0a3b8b9b163598a149e1690cba731d257
Qingyu Meng <rucmqy@gmail.com>
2016-03-31 15:23:11 +0800

    Update and rename 3-test.md to 3-test.py

--- For-Beginners/3-test.py
+++ For-Beginners/3-test.py
@@ -7,3 +7,24 @@
 3. 你知道怎么计算坐车去某个地方需要花多长时间吗？相应的公式（用文字表
 述）是“旅行时间等于距离除以速度”。编写一个程序，计算以80 km/h 的速
 度行驶200 km 需要花多长时间，并显示答案。
+
+import random
+secret = random.randint(1,99)
+guess = 0
+tries = 0
+print "This is a secret number game"
+print "number 1 to 99,can try 6 times"
+while guess != secret and tries < 6:
+    guess = input("input your number:")
+    if guess < secret:
+        print "it's low"
+    elif guess > secret:
+            print "it's too high"
+
+    tries = tries + 1
+
+if guess == secret:
+    print "Got it"
+else:
+    print "No More time"
+    print "the number is:",secret
